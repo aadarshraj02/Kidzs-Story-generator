9767d5772d7a315029a6b659253a9c89c31d9437
aadarshraj02 <aadarshraazz007@gmail.com>
2024-09-17 17:11:39 +0530

    imported and added Hero.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import { Button } from "@nextui-org/button";
 import Image from "next/image";
 import Header from "./_components/Header";
+import Hero from "./_components/Hero";
 
 export default function Home() {
   return (
     <div className="bg-[#cad3ff]">
       <Header />
+      <Hero />
     </div>
   );
 }
