5a04c718a3df516881c4fbba23f52c1972b8884a
aadarshraj02 <aadarshraazz007@gmail.com>
2024-09-17 21:54:05 +0530

    added and styled login page

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,5 +1,21 @@
 import { SignIn } from "@clerk/nextjs";
+import Image from "next/image";
 
 export default function Page() {
-  return <SignIn />;
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2">
+      <div>
+        <Image
+          src={"/assets/login.png"}
+          alt="login-image"
+          width={700}
+          height={400}
+          className="w-full"
+        />
+      </div>
+      <div className="flex items-center justify-center h-screen order-first md:order-last">
+        <SignIn />
+      </div>
+    </div>
+  );
 }
