8504331d73e847aa42385543a704db8f64a44d97
aadarshraj02 <aadarshraazz007@gmail.com>
2024-09-17 14:45:59 +0530

    added default code to check the setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,11 @@
+import { Button } from "@nextui-org/button";
 import Image from "next/image";
 
 export default function Home() {
-  return <div></div>;
+  return (
+    <div>
+      <h2>Trial and Setup phase</h2>
+      <Button>Trial</Button>
+    </div>
+  );
 }
