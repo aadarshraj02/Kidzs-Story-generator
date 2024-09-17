d70ab1bd9c75c6dce1d388f71fa95cfa72ba4af8
aadarshraj02 <aadarshraazz007@gmail.com>
2024-09-17 14:39:58 +0530

    wrapped in Provider

--- app/provider.tsx
+++ app/provider.tsx
@@ -1,8 +1,8 @@
 import { NextUIProvider } from "@nextui-org/react";
 import React from "react";
 
-const provider = ({ children }: { children: React.ReactNode }) => {
+const Provider = ({ children }: { children: React.ReactNode }) => {
   return <NextUIProvider>{children}</NextUIProvider>;
 };
 
-export default provider;
+export default Provider;
