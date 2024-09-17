b78340efbf26ebb9d5b31885c51189d7b672cc59
aadarshraj02 <aadarshraazz007@gmail.com>
2024-09-17 16:31:51 +0530

    added structure for navbar

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -1,5 +1,24 @@
+import {
+  Navbar,
+  NavbarBrand,
+  NavbarContent,
+  NavbarItem,
+  NavbarMenuToggle,
+  NavbarMenu,
+  NavbarMenuItem,
+} from "@nextui-org/navbar";
+import Image from "next/image";
+
 const Header = () => {
-  return <div></div>;
+  return (
+    <Navbar>
+      <NavbarContent>
+        <NavbarBrand>
+          <Image src={""} alt="" />
+        </NavbarBrand>
+      </NavbarContent>
+    </Navbar>
+  );
 };
 
 export default Header;
