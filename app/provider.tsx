98b329344c8be50aa313dba31bbf42d0f38354de
aadarshraj02 <aadarshraazz007@gmail.com>
2024-09-18 13:31:47 +0530

    imported and added header for all components

--- app/provider.tsx
+++ app/provider.tsx
@@ -1,10 +1,12 @@
 import { ClerkProvider } from "@clerk/nextjs";
 import { NextUIProvider } from "@nextui-org/react";
 import React from "react";
+import Header from "./_components/Header";
 
 const Provider = ({ children }: { children: React.ReactNode }) => {
   return (
     <ClerkProvider>
+      <Header />
       <NextUIProvider>{children}</NextUIProvider>
     </ClerkProvider>
   );
