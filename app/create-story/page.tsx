6d8ad5c74093d2c9eb8babdca7ca519e8cf277a3
aadarshraj02 <aadarshraazz007@gmail.com>
2024-09-18 14:29:02 +0530

    added background color

--- app/create-story/page.tsx
+++ app/create-story/page.tsx
@@ -1,5 +1,12 @@
 const CreateStory = () => {
-  return <div>CreateStory</div>;
+  return (
+    <div className="p-10 md:px-20 lg:px-40">
+      <h2 className="uppercase font-extrabold text-primary text-center text-[5vw]">
+        Create your story
+      </h2>
+      <p>Unlock your creativity with AI: Craft stories like never existed. Let our AI bring your imagination to life, one story at a time.</p>
+    </div>
+  );
 };
 
 export default CreateStory;
