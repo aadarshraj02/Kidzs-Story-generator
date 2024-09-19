3a64a5873a3b24ec34f84b191509ad3eef665623
aadarshraj02 <aadarshraazz007@gmail.com>
2024-09-19 22:20:17 +0530

    added p and styled the about

--- app/create-story/page.tsx
+++ app/create-story/page.tsx
@@ -4,7 +4,10 @@ const CreateStory = () => {
       <h2 className="uppercase font-extrabold text-primary text-center text-[5vw]">
         Create your story
       </h2>
-      <p>Unlock your creativity with AI: Craft stories like never existed. Let our AI bring your imagination to life, one story at a time.</p>
+      <p className="text-2xl text-primary text-center">
+        Unlock your creativity with AI: Craft stories like never existed. Let
+        our AI bring your imagination to life, one story at a time.
+      </p>
     </div>
   );
 };
