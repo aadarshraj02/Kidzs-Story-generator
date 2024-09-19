8fa305c81e2c81810d296fb8a5c66f886fa60505
aadarshraj02 <aadarshraazz007@gmail.com>
2024-09-19 22:28:30 +0530

    added text area for story input

--- app/create-story/_components/StorySubjectInput.tsx
+++ app/create-story/_components/StorySubjectInput.tsx
@@ -1,9 +1,19 @@
+import { Textarea } from "@nextui-org/input";
+
 const StorySubjectInput = () => {
   return (
     <div>
       <label className="font-bold text-2xl md:text-4xl text-primary" htmlFor="">
         1. Subject of the story
       </label>
+      <Textarea
+        placeholder="Write the subject of your story.."
+        size="lg"
+        classNames={{
+          input: "resize-y min-h-[200px] text-2xl p-5",
+        }}
+        className="mt-3 max-w-lg"
+      />
     </div>
   );
 };
