b006d62e94de818dd70316322e84e45c9102efad
aadarshraj02 <aadarshraazz007@gmail.com>
2024-09-19 22:32:53 +0530

    added function to get userSelection

--- app/create-story/page.tsx
+++ app/create-story/page.tsx
@@ -1,6 +1,15 @@
 import StorySubjectInput from "./_components/StorySubjectInput";
 
+export interface fieldData {
+  fieldName: string;
+  fiendValue: string;
+}
+
 const CreateStory = () => {
+  const onHandleUserSelection = (data: fieldData) => {
+    console.log(data);
+  };
+
   return (
     <div className="p-10 md:px-20 lg:px-40">
       <h2 className="uppercase font-extrabold text-primary text-center text-[5vw]">
@@ -11,7 +20,7 @@ const CreateStory = () => {
         our AI bring your imagination to life, one story at a time.
       </p>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-10 mt-14">
-        <StorySubjectInput />
+        <StorySubjectInput userSelection={onHandleUserSelection} />
       </div>
     </div>
   );
