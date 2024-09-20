4c9803bcac59abe4587a48853dad2949877ffa18
aadarshraj02 <aadarshraazz007@gmail.com>
2024-09-20 18:33:30 +0530

    displayed image and title for story type

--- app/create-story/_components/StoryType.tsx
+++ app/create-story/_components/StoryType.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import Image from "next/image";
 
 const StoryType = () => {
@@ -22,15 +24,21 @@ const StoryType = () => {
   return (
     <div>
       <label className="font-bold text-4xl text-primary">2.Story Type</label>
-      <div className="grid grid-cols-3 gap-3 ">
+      <div className="grid grid-cols-3 gap-3">
         {OptionList.map((item, index) => (
-          <div key={index}>
+          <div
+            key={index}
+            className="relative mt-3 grayscale hover:grayscale-0 cursor-pointer"
+          >
+            <h2 className="absolute bottom-5 text-white text-center w-full text-2xl">
+              {item.label}
+            </h2>
             <Image
               src={item.imageUrl}
               alt={item.label}
               width={200}
               height={500}
-              className="object-cover h-[260px] rounded-3xl"
+              className="object-cover h-[240px] rounded-3xl "
             />
           </div>
         ))}
