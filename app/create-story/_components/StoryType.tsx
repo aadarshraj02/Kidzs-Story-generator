96f0d2f471f31bf67de010db2aa5ec287953ad81
aadarshraj02 <aadarshraazz007@gmail.com>
2024-09-20 17:23:44 +0530

    added story option list

--- app/create-story/_components/StoryType.tsx
+++ app/create-story/_components/StoryType.tsx
@@ -1,4 +1,22 @@
 const StoryType = () => {
+  const OptionList = [
+    {
+      label: "Story Book",
+      imageUrl: "/assets/story.png",
+      isFree: true,
+    },
+    {
+      label: "Bed Story",
+      imageUrl: "/assets/bedstory.png",
+      isFree: true,
+    },
+    {
+      label: "Educational",
+      imageUrl: "/assets/educational.png",
+      isFree: true,
+    },
+  ];
+
   return <div>StoryType</div>;
 };
 
