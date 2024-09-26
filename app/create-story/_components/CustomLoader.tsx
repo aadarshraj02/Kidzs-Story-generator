e7950d6cf383e86a3421ec9b3c569a43a72b615a
aadarshraj02 <aadarshraazz007@gmail.com>
2024-09-26 18:46:04 +0530

    added functional component

---
+++ app/create-story/_components/CustomLoader.tsx
@@ -0,0 +1,12 @@
+import {
+  Modal,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+} from "@nextui-org/modal";
+const CustomLoader = () => {
+  return <div>CustomLoader</div>;
+};
+
+export default CustomLoader;
