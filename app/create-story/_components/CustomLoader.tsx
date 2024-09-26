589095d80311d6b0f05e11c3874464306d0a8f08
aadarshraj02 <aadarshraazz007@gmail.com>
2024-09-26 18:49:49 +0530

    added modal

--- app/create-story/_components/CustomLoader.tsx
+++ app/create-story/_components/CustomLoader.tsx
@@ -1,12 +1,39 @@
+import { Button } from "@nextui-org/button";
 import {
   Modal,
   ModalContent,
   ModalHeader,
   ModalBody,
   ModalFooter,
+  useDisclosure,
 } from "@nextui-org/modal";
+import { useEffect } from "react";
 const CustomLoader = () => {
-  return <div>CustomLoader</div>;
+  const { isOpen, onOpen, onOpenChange } = useDisclosure();
+  useEffect(() => {
+    onOpen();
+  }, []);
+
+  return (
+    <div>
+      <Modal isOpen={isOpen} onOpenChange={onOpenChange}>
+        <ModalContent>
+          {(onClose) => (
+            <>
+              <ModalHeader className="flex flex-col gap-1">
+                Modal Title
+              </ModalHeader>
+              <ModalBody>
+                <p></p>
+                <p></p>
+                <p></p>
+              </ModalBody>
+            </>
+          )}
+        </ModalContent>
+      </Modal>
+    </div>
+  );
 };
 
 export default CustomLoader;
