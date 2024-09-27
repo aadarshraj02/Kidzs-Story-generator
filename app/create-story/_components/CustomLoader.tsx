9996198eaa65ba125591f5240a82bd4d984ac547
aadarshraj02 <aadarshraazz007@gmail.com>
2024-09-27 20:43:39 +0530

    added conditional loading and formatted code

--- app/create-story/_components/CustomLoader.tsx
+++ app/create-story/_components/CustomLoader.tsx
@@ -9,7 +9,7 @@ import {
 } from "@nextui-org/modal";
 import Image from "next/image";
 import { useEffect } from "react";
-const CustomLoader = () => {
+const CustomLoader = ({ isLoading }: any) => {
   const { isOpen, onOpen, onOpenChange } = useDisclosure();
   useEffect(() => {
     onOpen();
@@ -17,26 +17,28 @@ const CustomLoader = () => {
 
   return (
     <div>
-      <Modal isOpen={isOpen} onOpenChange={onOpenChange}>
-        <ModalContent>
-          {(onClose) => (
-            <>
-              <ModalBody className="p-10 w-full flex items-center justify-center">
-                <Image
-                  src={"/assets/loader.gif"}
-                  alt="loader"
-                  width={300}
-                  height={300}
-                  className="w-[200px] h-[200px]"
-                />
-                <h2 className="text-2xl text-center text-bold text-primary">
-                  Please Wait, Story Generating...
-                </h2>
-              </ModalBody>
-            </>
-          )}
-        </ModalContent>
-      </Modal>
+      {isLoading && (
+        <Modal isOpen={isOpen} onOpenChange={onOpenChange}>
+          <ModalContent>
+            {(onClose) => (
+              <>
+                <ModalBody className="p-10 w-full flex items-center justify-center">
+                  <Image
+                    src={"/assets/loader.gif"}
+                    alt="loader"
+                    width={300}
+                    height={300}
+                    className="w-[200px] h-[200px]"
+                  />
+                  <h2 className="text-2xl text-center text-bold text-primary">
+                    Please Wait, Story Generating...
+                  </h2>
+                </ModalBody>
+              </>
+            )}
+          </ModalContent>
+        </Modal>
+      )}
     </div>
   );
 };
