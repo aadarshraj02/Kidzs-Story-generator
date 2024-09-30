38429d51613c9326baf3d439078299404e6b2372
aadarshraj02 <aadarshraazz007@gmail.com>
2024-09-30 18:50:06 +0530

    added prompt to generate image

--- app/api/generate-image/route.tsx
+++ app/api/generate-image/route.tsx
@@ -1,5 +1,32 @@
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
+import Replicate from "replicate";
 
 export async function POST(req: NextRequest) {
-  return NextResponse.json({ Data: "Hi" });
+  const replicate = new Replicate({
+    auth: process.env.REPLICATE_API_TOKEN,
+  });
+
+  const output: any = await replicate.run(
+    "bingbangboom-lab/flux-new-whimscape:2e8de10f217bc56da163a0204cf09f89995eaf643459014803fae79753183682",
+    {
+      input: {
+        model: "dev",
+        width: 856,
+        height: 1156,
+        prompt:
+          "a white-haired young woman wearing a flower crown, a very large fiery dragon, castle in the background, illustration in the style of WHMSCPE001",
+        lora_scale: 1,
+        num_outputs: 1,
+        aspect_ratio: "1:1",
+        output_format: "png",
+        guidance_scale: 3.5,
+        output_quality: 100,
+        prompt_strength: 0.8,
+        extra_lora_scale: 1,
+        num_inference_steps: 25,
+      },
+    }
+  );
+  console.log(output);
+  return NextResponse.json({ imageUrl: output[0] });
 }
